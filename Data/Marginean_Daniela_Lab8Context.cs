557bf5d3a5e4961574ecaeb51d8d793ac9a6f5fc
Daniela Marginean <dana.marginean@gmail.com>
2022-01-12 20:47:02 +0200

    Add project files.

---
+++ Data/Marginean_Daniela_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marginean_Daniela_Lab8.Models;
+
+namespace Marginean_Daniela_Lab8.Data
+{
+    public class Marginean_Daniela_Lab8Context : DbContext
+    {
+        public Marginean_Daniela_Lab8Context (DbContextOptions<Marginean_Daniela_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marginean_Daniela_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Marginean_Daniela_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Marginean_Daniela_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Marginean_Daniela_Lab8.Data;
+using Marginean_Daniela_Lab8.Models;
+
+namespace Marginean_Daniela_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Marginean_Daniela_Lab8.Data.Marginean_Daniela_Lab8Context _context;
+
+        public IndexModel(Marginean_Daniela_Lab8.Data.Marginean_Daniela_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
